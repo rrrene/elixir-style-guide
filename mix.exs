d86152066069eb8553f9fcef925c4ba3e3e6b280
René Föhring <rf@bamaru.de>
2016-02-11 23:02:03 +0100

    Add obelisk as a dependency

---
+++ mix.exs
@@ -0,0 +1,33 @@
+defmodule ElixirStyleGuide.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :elixir_style_guide,
+     version: "0.0.1",
+     elixir: "~> 1.2",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    [applications: [:logger]]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
+  defp deps do
+  [{ :obelisk, "~> 0.7" },
+   { :yamerl, github: "yakaz/yamerl"}]
+  end
+end
